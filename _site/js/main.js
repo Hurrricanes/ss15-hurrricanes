e420e19b4e56b865e7dfbeee75dbd15b6cf8a7e2
Praminda Jayawardana <pramindabj@gmail.com>
2015-01-24 16:29:03 +0530

    Added index page template

--- _site/js/main.js
+++ _site/js/main.js
@@ -1,11 +1,23 @@
 $(function () {
 	function HackboxViewModel () {
-		
-	}
-	// Register the callback to be fired every time auth state changes
-	var user = getAuth();
+		var self = this;
+		self.isLoggedIn = ko.observable(false);
 
-	if (!user) {
-		window.location.replace("login.html");
+		// Register the callback to be fired every time auth state changes
+		var user = getAuth();
+
+		if (user == undefined || user == null) {
+			window.location.replace("login.html");
+			self.isLoggedIn(false);
+		} else {
+			self.isLoggedIn(true);
+		}
+
+		self.signout = function () {
+			logout();
+			window.location.replace("login.html");
+		}
 	}
+	ko.applyBindings(new HackboxViewModel());
+	
 });
