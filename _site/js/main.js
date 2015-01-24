f49b88c1845f5a4181974af352fc2845fd58d2ef
Praminda Jayawardana <pramindabj@gmail.com>
2015-01-25 00:05:47 +0530

    Added hackbox list view

--- _site/js/main.js
+++ _site/js/main.js
@@ -3,6 +3,7 @@ $(function () {
 		var self = this;
 		self.isLoggedIn = ko.observable(false);
 		self.isConnected = ko.observable(false);
+		self.users = ko.observableArray([]);
 
 		// Register the callback to be fired every time auth state changes
 		var user = getAuth();
@@ -22,14 +23,35 @@ $(function () {
 
 		// connect user to the network
 		self.connect = function() {
-			connect(); // connect to firebase
-			self.isConnected(true);
+			// connect to firebase
+			connect(function () {
+				self.isConnected(true);
+				// register listeners to hackbox changes
+				onHackNetChanged(self.hackBoxConnectedCallback, self.hackBoxChangedCallback, self.hackBoxDisconnectedCallback);
+			}, function () {
+				self.isConnected(false);
+			});
 		}
 
 		// disconnect user from the network
 		self.disconnect = function() {
 			disconnect(); // disconnect from firebase
 			self.isConnected(false);
+		}
+
+		// callback function to detect new user connected to the network
+		self.hackBoxConnectedCallback = function (user) {
+			self.users.push(user.val());
+		}
+
+		// callback function to detect changes of a user
+		self.hackBoxChangedCallback = function (user) {
+			
+		}
+
+		// callback function to detect disconnection of a user
+		self.hackBoxDisconnectedCallback = function (user) {
+			
 		}
 	}
 	ko.applyBindings(new HackboxViewModel());
