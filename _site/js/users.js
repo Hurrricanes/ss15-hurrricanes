5f264c88c21f7e84ebb4a78d56d6cbc408209f22
Yehan Pemarathne <yehancha@gmail.com>
2015-01-24 09:26:51 +0530

    Authentication methods changed by user types.
    Existing users will be updated and new user will be added with 1000
    coins.

--- _site/js/users.js
+++ _site/js/users.js
@@ -55,8 +55,19 @@ function logout() {
 function authDataCallback(authData) {
   if (authData) {
     console.log("User " + authData.uid + " is logged in with " + authData.provider);
-    // save the user's profile into Firebase so we can list users
-    ref.child("users").child(authData.uid).set(authData);
+    
+    // Check whether the user exixsts.
+    ref.child("users").child(authData.uid).once("value", function (snapshot) {
+      if (snapshot.exists()) {
+        // If the user exists we update the data.
+        ref.child("users").child(authData.uid).update(authData);
+      } else {
+        // otherwise we add a new user
+        ref.child("users").child(authData.uid).set(authData);
+        ref.child("users").child(authData.uid).child("gamedata/coins").set(1000);
+      }
+    });
+    
     $("#login").hide();
     $("#logout").show();
   } else {
