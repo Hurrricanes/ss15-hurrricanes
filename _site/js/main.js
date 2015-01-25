b2c45b851afd225ba728b7c0890fe3b39989640e
anushkas <anushkasandaruwan1@gmail.com>
2015-01-25 08:57:20 +0530

    changing terminal functions

--- _site/js/main.js
+++ _site/js/main.js
@@ -80,14 +80,14 @@ $(function () {
 jQuery(function($, undefined) {
     $('#terminal').terminal(function(command, term) {
         if (command !== '') {
-            try {
-                var result = window.eval(command);
-                if (result !== undefined) {
-                    term.echo(new String(result));
-                }
-            } catch(e) {
-                term.error(new String(e));
-            }
+//            try {
+//                var result = window.eval(command);
+//                if (result !== undefined) {
+//                    term.echo(new String(result));
+//                }
+//            } catch(e) {
+//                term.error(new String(e));
+//            }
         } else {
            term.echo('');
         }
